cee70470df05610753de34d9d18361d63e973db8
Felipe Méndez <fmendez1212@existaya.com>
2013-12-03 14:05:03 -0500

    touch events
    broadcast the touch events from the phone to the desktop clients

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
-var io = require('socket.io'),
-  connect = require('connect');
+var   io = require('socket.io')
+  	, fs = require('fs')
+	, connect = require('connect');
+
 
 var app = connect().use(connect.static('public')).listen(3000);
-var chat_room = io.listen(app);
+var controller = io.listen(app);
 
-chat_room.sockets.on('connection', function (socket) {
+function handler (req, res) {
+  fs.readFile(__dirname + '/public/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+controller.sockets.on('connection', function (socket) {
   socket.emit('entrance', {message: 'Welcome to the chat room!'});
 
   socket.on('disconnect', function  () {
-    chat_room.sockets.emit('exit', {message: 'A chatter has disconnected.'});
+    controller.sockets.emit('exit', {message: 'A chatter has disconnected.'});
   });
 
-  socket.on('chat', function  (data) {
-    chat_room.sockets.emit('chat', {message: '# ' + data.message});
+  socket.on('mouseMove', function (mouseX, mouseY) {
+    controller.sockets.emit('mouseMoved', mouseX, mouseY);
   });
 
-  chat_room.sockets.emit('entrance', {message: 'A new chatter is online.'});
+  controller.sockets.emit('entrance', {message: 'A new chatter is online.'});
 });
